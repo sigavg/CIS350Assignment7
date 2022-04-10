75031657d6ebdd855a01ed6ae4e0ddf16524ed71
sigavg <lesimplu@gmail.com>
2022-04-10 17:29:43 -0500

    1

---
+++ Assignment7/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿/* Luke Lesimple
+ * Assignment 7
+ * enemy movement
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody erb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        erb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 lookdir = (player.transform.position - transform.position).normalized;
+        erb.AddForce(lookdir * speed);  
+    }
+    
+    
+}
---
+++ Assignment7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿/* Luke Lesimple
+ * Assignment 7
+ * Controls player movement
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody prb;
+    public float speed = 5.0f;
+    private float forwardInput;
+
+    private GameObject focalpt;
+
+    public bool hasPowerUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prb = GetComponent<Rigidbody>();
+        focalpt = GameObject.FindGameObjectWithTag("FocalPoint");
+    }
+
+    private void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        prb.AddForce(focalpt.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Player collided with: " + collision.gameObject.name +
+                " with powerup set to " + hasPowerUp);
+        }
+    }
+
+}
